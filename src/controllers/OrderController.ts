21f8045b6175e37b05df96e0b81774e0b83f168b
HuynhLeo <nhanvl112@gmail.com>
2024-12-10 10:24:23 +0700

    first commit

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -81,7 +81,7 @@ const createCheckoutSession = async (req: Request, res: Response) => {
     const newOrder = new Order({
       restaurant: restaurant,
       user: req.userId,
-      status: "placed",
+      status: "Đã đặt",
       deliveryDetails: checkoutSessionRequest.deliveryDetails,
       cartItems: checkoutSessionRequest.cartItems,
       createdAt: new Date(),
